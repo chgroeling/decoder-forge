77d7eeee09828485516c273eae867b0ccf9c6a47
Christian Gröling <contact@christiangroeling.de>
2025-05-11 08:26:11 +0200

    feature: Add file object to Printer class

--- decoder_forge/external/printer.py
+++ decoder_forge/external/printer.py
@@ -2,5 +2,21 @@ from decoder_forge.i_printer import IPrinter
 
 
 class Printer(IPrinter):
+    """
+    A simple Printer implementation that writes output to a provided file-like object.
+
+    This class implements the IPrinter interface and writes each output string followed
+    by a newline to the specified file object.
+
+    Example:
+        >>> import sys
+        >>> printer = Printer(sys.stdout)
+        >>> printer.print("Hello, World!")
+    """
+
+    def __init__(self, file_object):
+        self._file_object = file_object
+
     def print(self, out: str):
-        print(out)
+        self._file_object.write(out)
+        self._file_object.write("\n")
--- decoder_forge/main.py
+++ decoder_forge/main.py
@@ -1,9 +1,13 @@
 import click
 import logging
+import sys
+
+from typing import Optional
 from decoder_forge.uc_show_decode_tree import uc_show_decode_tree
 from decoder_forge.external.printer import Printer
 from decoder_forge.external.template_engine import TemplateEngine
 from decoder_forge.uc_generate_code import uc_generate_code
+from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
 
@@ -27,42 +31,89 @@ def cli(ctx, verbose):
     ctx.obj["verbosity"] = verbose
 
 
+@contextmanager
+def open_output_stream(output_file: Optional[str]):
+    """
+    Context manager for opening an output stream.
+
+    This function yields sys.stdout if no output file is specified.
+    Otherwise, it opens the specified file for writing and yields the file handle.
+
+    Args:
+        output_file (Optional[str]): Path to the output file. If None, sys.stdout is used.
+
+    Yields:
+        TextIO: A writable file-like object.
+
+    Example:
+        with open_output_stream('output.txt') as stream:
+            stream.write("Hello World!")
+    """
+
+    if output_file is None:
+        yield sys.stdout
+    else:
+        with open(output_file, "w") as f:
+            yield f
+
+
 @cli.command()
 @click.argument("INPUT_PATH", type=str)
 @click.option(
     "--decoder_width",
-    help="Target bit width; patterns are extended to this width before decoding (default: 32)",
+    help="Target bit width; patterns are extended to this width before decoding "
+    + "(default: 32)",
     default=32,
     type=int,
 )
+@click.option(
+    "--output_file",
+    help="Output file to write the generated code. Defaults to None, which outputs to "
+    + "stdout.",
+    default=None,
+    type=str,
+)
 @click.pass_context
-def generate_code(self, input_path: str, decoder_width: int):
-    """Generate decoder code from YAML pattern definitions.
+def generate_code(
+    self, input_path: str, decoder_width: int, output_file: Optional[str]
+):
+    """Generate decoder code from YAML instruction patterns.
 
-    This command reads a YAML file from the specified INPUT_PATH, which is expected to
-    contain binary instruction definitions.It the generates decoder code based on the
-    contained patterns using a template engine, and prints the generated code line by
-    line to the standard output.
+    This command reads a YAML file from the provided INPUT_PATH which should contain
+    binary instruction definitions. It then generates decoder code using a template
+    engine and prints the output either to stdout or an output file if provided.
 
-    INPUT_PATH: The file path to a YAML file containing pattern definitions.
+    Args:
+        input_path (str): The file path to the YAML file containing instruction
+          definitions.
+        decoder_width (int): The target bit width for extending patterns
+          (default is 32).
+        output_file (Optional[str]): Optional file path to write the generated code.
+
+    Raises:
+        IOError: If reading the input file or writing to the output file fails.
 
     Example:
-        $ python cli.py generate_code patterns.yaml
+        $ python cli.py generate_code patterns.yaml --decoder_width 32
+          --output_file decoder.py
     """
+
     yaml_buf = ""
     with open(input_path, "r", encoding="utf-8") as fp:
         yaml_buf = fp.read()
 
-    printer = Printer()
     tengine = TemplateEngine()
-    uc_generate_code(printer, tengine, yaml_buf, decoder_width)
+    with open_output_stream(output_file) as f:
+        printer = Printer(f)
+        uc_generate_code(printer, tengine, yaml_buf, decoder_width)
 
 
 @cli.command()
 @click.argument("INPUT_PATH", type=str)
 @click.option(
     "--decoder_width",
-    help="Target bit width; patterns are extended to this width before decoding (default: 32)",
+    help="Target bit width; patterns are extended to this width before decoding "
+    + "(default: 32)",
     default=32,
     type=int,
 )
@@ -85,7 +136,7 @@ def show_tree(ctx, input_path: str, decoder_width: int):
     with open(input_path, "r", encoding="utf-8") as fp:
         yaml_buf = fp.read()
 
-    printer = Printer()
+    printer = Printer(sys.stdout)
     uc_show_decode_tree(printer, yaml_buf, decoder_width)
 
 
